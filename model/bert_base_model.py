b1667b948c6e1ed053eeaacb3197d7cd8ccfc3af
TiAmoLip <1239261229@qq.com>
2023-12-13 14:25:31 +0800

    simplified model

--- model/bert_base_model.py
+++ model/bert_base_model.py
@@ -17,7 +17,7 @@ class BertBasedModel(nn.Module):
             self.bert = BertModel.from_pretrained("bert-base-chinese")
             self.bert.save_pretrained(path)
         self.bert.requires_grad_(False)
-        self.att = nn.MultiheadAttention(768,8)
+        # self.att = nn.MultiheadAttention(768,8)
         self.intent = IntentAugment(tag_classes = tag_classes)
         self.loss = nn.CrossEntropyLoss(ignore_index = config.tag_pad_idx)
 
@@ -30,8 +30,8 @@ class BertBasedModel(nn.Module):
 
         x = self.bert(input_ids=input_ids,attention_mask = tag_mask)
         last_hidden = x['last_hidden_state'] # 这里得到的就是每个词的表示和整个语义信息的高维表示(bs,seq_len,768), (bs, 768)
-        h_prime = self.att(last_hidden,last_hidden,last_hidden)[0]
-        logits = self.intent(last_hidden,h_prime)# bs, seq_len,tag_classes, 这里的None本来应该是h_prime
+        # h_prime = self.att(last_hidden,last_hidden,last_hidden)[0]
+        logits = self.intent(last_hidden,None)# bs, seq_len,tag_classes, 这里的None本来应该是h_prime
         
         logits += (1-tag_mask).unsqueeze(-1).repeat(1,1,self.num_tags)* -1e32
         prob = torch.softmax(logits,dim = -1)
@@ -81,15 +81,15 @@ class BertBasedModel(nn.Module):
 class IntentAugment(nn.Module):
     def __init__(self, tag_classes) -> None:
         super().__init__()
-        self.att = nn.MultiheadAttention(768*2,8)
+        # self.att = nn.MultiheadAttention(768*2,8)
         self.mlp = nn.Sequential(
-            nn.Linear(768*3,768),
-            # nn.Linear(768,768),
+            # nn.Linear(768*3,768),
+            nn.Linear(768,768),
             nn.LeakyReLU(0.2),
             nn.Linear(768,tag_classes)
         )
     def forward(self,bert_hidden, att_hidden):
-        h_prime_prime = torch.cat([bert_hidden,att_hidden],dim = -1)
-        e_s = self.att(h_prime_prime,h_prime_prime,h_prime_prime)[0]
-        h_prime_prime = torch.cat([e_s,att_hidden],dim = -1)
-        return self.mlp(h_prime_prime)
+        # h_prime_prime = torch.cat([bert_hidden,att_hidden],dim = -1)
+        # e_s = self.att(h_prime_prime,h_prime_prime,h_prime_prime)[0]
+        # h_prime_prime = torch.cat([e_s,att_hidden],dim = -1)
+        return self.mlp(bert_hidden)
