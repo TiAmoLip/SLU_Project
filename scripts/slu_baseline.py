53e148b7aa5e32d02f3386eee05cd2190f01217d
TiAmoLip <1239261229@qq.com>
2023-12-13 14:39:11 +0800

    fix bugs

--- scripts/slu_baseline.py
+++ scripts/slu_baseline.py
@@ -41,8 +41,9 @@ args.tag_pad_idx = Example.label_vocab.convert_tag_to_idx(PAD)#0
 
 
 if args.load_embedding:
-    Example.word2vec.load_embeddings(model.word_embed, Example.word_vocab, device=device)
+    
     model = SLUTagging(args).to(device)
+    Example.word2vec.load_embeddings(model.word_embed, Example.word_vocab, device=device)
 else:
     model = BertBasedModel(args).to(device)
 if args.testing:
